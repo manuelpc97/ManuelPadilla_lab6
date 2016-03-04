d8f7ef4b684d32519e282c87983b4d159f4a7084
manuelpc97 <manuelpc97@unitec.edu>
2016-03-04 15:49:23 -0600

    Elimina usuarios

--- main.cpp
+++ main.cpp
@@ -19,8 +19,10 @@ int main(int argc, char*argv[]){
 	vector <games> juegos;	
 	bool seguir = true;
 	int menu, menu2, pos,id;
+	int year;
+	string desa, gen;
 	string nombre;
-	string contra;
+	string contra, contra2, contraNueva;
 	bool verificar = false;
 
 	while(seguir){
@@ -62,9 +64,23 @@ int main(int argc, char*argv[]){
 						cout<<"El juego no existe"<<endl;
 					}								
 				}else if(menu2 == 2){
-						
+					cout<<"Ingrese la id del juego: "<<endl;
+					cin>>id;
+					if(personas[pos].deleteGame(id)){
+						cout<<"El juego ha sido borrado."<<endl;
+					}else{
+						cout<<"El juego no debe existir, ERROR"<<endl;
+					}	
 				}else if(menu2 == 3){
-
+					cout<<"Ingrese su antigua contraseña."<<endl;
+					cin>>contra2;
+					if(contra2.compare(contra) == 0){
+						cout<<"Ingrese su nueva contraseña"<<endl;
+						cin>>contraNueva;
+						personas[pos].setPassword(contraNueva);
+					}else{
+						cout<<"Contraseña no valida."<<endl;
+					}			
 				}else{
 					cout<<"Opcion incorrecta"<<endl;
 				}
@@ -72,23 +88,78 @@ int main(int argc, char*argv[]){
 				cout<<"El usuario no existe."<<endl;
 			}
 		}else if(menu==3){
-
+			cout<<"Ingrese el nombre del usuario que desea eliminar"<<endl;
+			cin>>nombre;
+			
+			for(int i = 0; i < personas.size(); i++){
+				if(personas[i].getName().compare(nombre)==0){
+					pos = i;
+					verificar = true;
+				}
+			}
+			
+			if(verificar){
+				personas.erase(personas.begin()+pos);
+			}else{
+				cout<<"Este usuario no existe."<<endl;
+			}	
 		}else if(menu==4){
-
+			cout<<"Ingrese un id"<<endl;
+			cin>>id;
+			cout<<"INgrese un nombre"<<endl;
+			cin>>nombre;
+			cout<<"Ingrese un año"<<endl;
+			cin>>year;
+			cout<<"Ingrese los desarrolladores"<<endl;
+			cin>>desa;
+			cout<<"Ingrese el genero"<<endl;
+			cin>>gen;
+			
+			for(int i = 0; i < juegos.size(); i++){
+				if(juegos[i].getId() == id){
+					verificar = true;
+				}
+			}
+			if(verificar == false){
+				games a(id,nombre,year,desa,gen);
+				juegos.push_back(a);
+			}else{
+				cout<<"Ya existe un juego con esta ID"<<endl;
+			}	
 		}else if(menu == 5){
+			cout<<"Ingrese el id del juego que desea eliminar: "<<endl;
+			cin>>id;
 
+			for(int i = 0; i < juegos.size(); i++){
+				if(id == juegos[i].getId()){
+					verificar = true;
+					pos = i;
+				}
+			}
+
+			if(verificar){
+				juegos.erase(juegos.begin()+pos);	
+			}else{
+				cout<<"El juego no existe"<<endl;
+			}
 		}else if(menu == 6){
 			cout<<"----Usuarios-----"<<endl;
 			for(int  i = 0; i < personas.size(); i++){
 				cout<<i+1<<"--"<<personas[i].toString()<<endl;
+				cout<<"************************"<<endl;
 			}
 		}else if(menu == 7){
-		
+			cout<<"--------Juegos-----"<<endl;
+			for(int i = 0; i<juegos.size(); i++){
+				cout<<juegos[i].toString()<<endl;
+				cout<<"***************************************"<<endl;
+			}
 		}else if(menu == 8){
 			seguir = false;
 		}else{
 			cout<<"Opcion incorrecta"<<endl;
 		}
+		verificar = false;
 	}
 	return 0;
 }
